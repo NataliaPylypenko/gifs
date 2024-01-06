1be3858f1069f7d20165f18b9efc9df8b11e8560
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-06 16:01:57 +0200

    query for not found

--- src/js/modules/getGifs.js
+++ src/js/modules/getGifs.js
@@ -5,7 +5,6 @@ import {displayGifs} from "@/js/modules/displayGifs";
 export const getRandomGifs = () => {
     fetchData(`${config.apiUrl}/v1/gifs/trending?api_key=${config.apiKey}&limit=15`)
         .then(response => {
-            // console.log(response.data);
             displayGifs(response.data);
         })
         .catch(err => console.error(err));
@@ -14,9 +13,23 @@ export const getRandomGifs = () => {
 export const searchGifs = (e) => {
     let searchTerm = e.target.value;
 
+    if (!searchTerm.trim()) {
+        getRandomGifs();
+        return;
+    }
+
     fetchData(`${config.apiUrl}/v1/gifs/search?api_key=${config.apiKey}&q=${searchTerm}&limit=15`)
         .then(response => {
-            displayGifs(response.data);
+            if (response.data.length === 0) {
+                return fetchData(`${config.apiUrl}/v1/gifs/search?api_key=${config.apiKey}&q=not+found&limit=1`);
+            } else {
+                displayGifs(response.data);
+            }
+        })
+        .then(randomGifResponse => {
+            if (randomGifResponse && randomGifResponse.data) {
+                displayGifs(randomGifResponse.data);
+            }
         })
         .catch(err => console.error(err));
 };
