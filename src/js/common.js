d3bea245eb8e93aebab44cede50da33110bf2efc
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-08 13:39:14 +0200

    get data after a little period of time

--- src/js/common.js
+++ src/js/common.js
@@ -4,8 +4,16 @@ import '../scss/style.scss';
 import refs from "@js/modules/refs";
 import { getRandomGifs, searchGifs } from "@js/modules/getGifs";
 
+let searchTimeout;
+
 window.addEventListener('DOMContentLoaded', () => {
     getRandomGifs();
 });
 
-refs.searchInput.addEventListener('input', searchGifs);
+refs.searchInput.addEventListener('input', (e) => {
+    clearTimeout(searchTimeout);
+
+    searchTimeout = setTimeout(() => {
+        searchGifs(e);
+    }, 1500);
+});
