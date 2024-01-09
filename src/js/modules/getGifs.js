d3f89a72d3df1fe270629e8886d992e3558c14c4
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-09 11:02:49 +0200

    show more

--- src/js/modules/getGifs.js
+++ src/js/modules/getGifs.js
@@ -1,30 +1,39 @@
 import config from "@js/modules/config";
+import refs from "@js/modules/refs";
 import {fetchData} from "@js/modules/fetchData";
 import {displayGifs} from "@js/modules/displayGifs";
 
+export const getGifs = (e) => {
+    let searchTerm = refs.searchInput.value;
+
+    console.log('searchTerm', searchTerm);
+    console.log('offset', refs.offset);
+
+    if (searchTerm.trim()) {
+        searchGifs(searchTerm);
+        return;
+    }
+
+    getRandomGifs();
+};
+
 export const getRandomGifs = () => {
-    fetchData(`${config.apiUrl}/v1/gifs/trending?api_key=${config.apiKey}&limit=15`)
+    fetchData(`${config.apiUrl}/v1/gifs/trending?api_key=${config.apiKey}&limit=${refs.limit}&offset=${refs.offset}`)
         .then(response => {
             displayGifs(response.data);
+            refs.offset += refs.limit;
         })
         .catch(err => console.error(err));
 };
 
-export const searchGifs = (e) => {
-    let searchTerm = e.target.value;
-    console.log(searchTerm);
-
-    if (!searchTerm.trim()) {
-        getRandomGifs();
-        return;
-    }
-
-    fetchData(`${config.apiUrl}/v1/gifs/search?api_key=${config.apiKey}&q=${searchTerm}&limit=15`)
+const searchGifs = (searchTerm) => {
+    fetchData(`${config.apiUrl}/v1/gifs/search?api_key=${config.apiKey}&q=${searchTerm}&limit=${refs.limit}&offset=${refs.offset}`)
         .then(response => {
             if (response.data.length === 0) {
                 return fetchData(`${config.apiUrl}/v1/gifs/search?api_key=${config.apiKey}&q=not+found&limit=9`);
             } else {
                 displayGifs(response.data);
+                refs.offset += refs.limit;
             }
         })
         .then(notFoundGifResponse => {
--- src/js/modules/refs.js
+++ src/js/modules/refs.js
@@ -1,6 +1,9 @@
 const refs = {
     gifContainer: document.querySelector('#gifContainer'),
     searchInput: document.querySelector('#searchInput'),
+    showMore: document.querySelector('#showMore'),
+    offset: 0,
+    limit: 12,
 };
 
 export default refs;
