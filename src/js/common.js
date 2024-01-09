3aa9ea59ad144ea003941f948f6a916cd45f135f
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-09 13:59:07 +0200

    keydown event

--- src/js/common.js
+++ src/js/common.js
@@ -11,3 +11,5 @@ window.addEventListener('DOMContentLoaded', getRandomGifs);
 refs.showMore.addEventListener('click', handleShowMore);
 
 refs.searchInput.addEventListener('input', debounce(handleInput, 1500));
+
+refs.searchInput.addEventListener('keydown', (e) => (e.key === 'Enter') && e.preventDefault());
